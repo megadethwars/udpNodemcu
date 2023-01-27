a367389eb95f06ee6dd22e7170bfd553c522015f
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2023-01-26 19:32:18 -0600

    V1

--- UDPnodemcu.ino
+++ UDPnodemcu.ino
@@ -15,6 +15,29 @@
 #define STASSID "empires"
 #define STAPSK  "44863333"
 #endif
+#include "ArduPID.h"
+///ports motor
+ int pwmMotorA = 5;
+ int pwmMotorB = 4;
+ int dirMotorA = 0;
+ int dirMotorB = 2;
+#define BUTTON 13 //botón en GPIO13 (D7)
+
+//pid
+ArduPID myController;
+double input;
+double output;
+volatile int count = 0;
+int velocidad=0;
+
+
+/////PID///
+// Arbitrary setpoint and gains - adjust these as fit for your project:
+double setpoint = 0;
+double p = 0.01;
+double i = 0.00005;
+double d = 0.0;
+
 
 unsigned int localPort = 8888;      // local port to listen on
 
@@ -24,6 +47,24 @@ char  ReplyBuffer[] = "acknowledged\r\n";       // a string to send back
 String datReq;
 WiFiUDP Udp;
 unsigned long startTime;
+
+void ICACHE_RAM_ATTR ISRoutine ();
+
+void setupPID(){
+
+    myController.begin(&input, &output, &setpoint, p, i, d);
+
+  // myController.reverse()               // Uncomment if controller output is "reversed"
+    //myController.setSampleTime(20);      // OPTIONAL - will ensure at least 10ms have past between successful compute() calls
+    myController.setOutputLimits(0, 2000);
+    //myController.setBias(255.0 / 2.0); /// una especie de offset, un 0 referencial, relativo
+    myController.setWindUpLimits(-2000, 2000); // Groth bounds for the integral term to prevent integral wind-up
+    
+    myController.start();
+    // myController.reset();               // Used for resetting the I and D terms - only use this if you know what you're doing
+    // myController.stop();                // Turn off the PID controller (compute() will not do anything until start() is called)
+ }
+
 
 void connectWifi(){
     WiFi.mode(WIFI_STA);
@@ -42,6 +83,18 @@ void setup() {
   Serial.begin(115200);
   connectWifi();
   startTime = millis();
+
+  analogWriteFreq(100);
+  analogWriteRange(2047);
+ 
+  pinMode(pwmMotorB, OUTPUT);
+  pinMode(dirMotorB, OUTPUT);
+  digitalWrite(dirMotorB, LOW);
+
+  pinMode(BUTTON, INPUT_PULLUP);
+ attachInterrupt(digitalPinToInterrupt(BUTTON),ISRoutine,RISING);
+
+ setupPID();
 }
 
 void Send(String estatus,int cuenta){
@@ -57,11 +110,11 @@ void Send(String estatus,int cuenta){
   }
 
 void receiveData(int pktsize){
-    Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n",
-                  pktsize,
-                  Udp.remoteIP().toString().c_str(), Udp.remotePort(),
-                  Udp.destinationIP().toString().c_str(), Udp.localPort(),
-                  ESP.getFreeHeap());
+    //Serial.printf("Received packet of size %d from %s:%d\n    (to %s:%d, free heap = %d B)\n",
+    //              pktsize,
+    //              Udp.remoteIP().toString().c_str(), Udp.remotePort(),
+    //              Udp.destinationIP().toString().c_str(), Udp.localPort(),
+    //              ESP.getFreeHeap());
 
     // read the packet into packetBufffer
     int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
@@ -75,8 +128,8 @@ void receiveData(int pktsize){
       cadenaString += packetBuffer[i];
     }
     */
-    Serial.println("Contents:");
-    Serial.println(datReq);
+    //Serial.println("Contents:");
+    //Serial.println(datReq);
 
     String CMD="";
     long DATA=0;
@@ -85,6 +138,7 @@ void receiveData(int pktsize){
         if (datReq.substring(i, i+1) == "|") {
             CMD = datReq.substring(0, i);
             DATA = datReq.substring(i+1).toInt();
+            
               break;
         }
     }
@@ -92,8 +146,9 @@ void receiveData(int pktsize){
   
     if(CMD=="S"){
        //Start();
+       setpoint=(double)DATA;
        Send("Start OK",1);
-       Serial.println("Start ok");
+       //Serial.println("Start ok");
     }
 
   if(CMD=="ST"){
@@ -120,8 +175,8 @@ void loop() {
   if (packetSize) {
     receiveData(packetSize);
   }
-
-
+  
+  /*
   if (millis() - startTime >= 10000) { 
     // Si ha pasado 1 segundo (1000 milisegundos) desde startTime
     // Your code here
@@ -130,9 +185,35 @@ void loop() {
     }
     startTime = millis(); // reiniciar el contador
   }
+  */
 
   
+  
 
+  if (millis() - startTime >= 250) { 
+
+    velocidad = count;
+    input=(double)velocidad;
+    count=0;
+    startTime = millis(); // reiniciar el contador
+  }
+  myController.compute();
+  
+  analogWrite(pwmMotorB, (int)output);
+
+
+   Serial.print(setpoint);
+   Serial.print(",");
+   Serial.print(input);
+   Serial.print(",");
+   Serial.print(output);
+   Serial.print(",");
+   Serial.println(0);
+  
+}
+
+void ISRoutine () {
+  count=count+1;
 }
 
 /*
